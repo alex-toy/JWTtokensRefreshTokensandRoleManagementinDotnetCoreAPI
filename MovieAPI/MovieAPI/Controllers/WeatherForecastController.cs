9a28ee05be8cc065276c738c8b256f0aec550e21
alex-toy <alexei.80@hotmail.fr>
2024-02-21 21:03:41 +0100

    identity

--- MovieAPI/MovieAPI/Controllers/WeatherForecastController.cs
+++ MovieAPI/MovieAPI/Controllers/WeatherForecastController.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace MovieAPI.Controllers
+namespace YtMovieApis.Controllers
 {
     [ApiController]
     [Route("[controller]")]
@@ -8,8 +8,8 @@ namespace MovieAPI.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
 
         private readonly ILogger<WeatherForecastController> _logger;
 
@@ -18,7 +18,7 @@ namespace MovieAPI.Controllers
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
+        [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
--- README.md
+++ README.md
@@ -3,12 +3,14 @@
 This project is about creating dotnet 6 web apis with jwt authentication, refresh tokens and role-base authorization in .net 6. We will implement login, signup and change password module with the help of asp.net core identity. We will implement the functionality of refresh tokens in dotnet core 6 apis. So that we can maintain login sessions for longer period of time.
 
 
-## MagicVilla_Web
+## Package Nuget
+```
+Microsoft.EntityFrameworkCore.SqlServer
+Microsoft.EntityFrameworkCore.Tools
+Microsoft.AspNetCore.Identity.EntityFrameworkCore
+Microsoft.AspNetCore.Authentication.JwtBearer
+```
 
-- install packages
-```
-Microsoft.AspNetCore.Authentication.OpenIdConnect
-```
 - run the Identity, API and Web. Then go to login 
 <img src="/pictures/identity_server2.png" title="identity server"  width="900">
 
