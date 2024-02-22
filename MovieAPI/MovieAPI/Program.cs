41ca081e67b21be9322364169f604d459a0b2717
alex-toy <alexei.80@hotmail.fr>
2024-02-22 19:34:26 +0100

    extensions

--- MovieAPI/MovieAPI/Program.cs
+++ MovieAPI/MovieAPI/Program.cs
@@ -1,55 +1,18 @@
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
-using YtMovieApis.Models.Domain;
-using YtMovieApis.Repositories.Abstract;
-using YtMovieApis.Repositories.Domain;
+using MovieAPI;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-// For Entity Framework  
-builder.Services.AddDbContext<DatabaseContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("conn")));
+builder.ConfigureDbContext();
+builder.ConfigureIdentity();
+builder.ConfigureAuthentication();
+builder.ConfigureServices();
 
-// For Identity  
-builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
-    .AddEntityFrameworkStores<DatabaseContext>()
-    .AddDefaultTokenProviders();
 
-// Adding Authentication  
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-})
 
-// Adding Jwt Bearer  
-.AddJwtBearer(options =>
-{
-    options.SaveToken = true;
-    options.RequireHttpsMetadata = false;
-    options.TokenValidationParameters = new TokenValidationParameters()
-    {
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidAudience = builder.Configuration["JWT:ValidAudience"],
-        ValidIssuer = builder.Configuration["JWT:ValidIssuer"],
-        ClockSkew = TimeSpan.Zero,
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Secret"]))
-    };
-});
 
-builder.Services.AddTransient<ITokenService, TokenService>();
 
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
+WebApplication app = builder.Build();
 
 app.UseHttpsRedirection();
 app.UseCors(options =>
---
+++ MovieAPI/MovieAPI/WebApplicationBuilderExtension.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using YtMovieApis.Models.Domain;
+using YtMovieApis.Repositories.Abstract;
+using YtMovieApis.Repositories.Domain;
+
+namespace MovieAPI
+{
+    public static class WebApplicationBuilderExtension
+    {
+        public static void ConfigureDbContext(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddDbContext<DatabaseContext>(options =>
+            {
+                string connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+                options.UseSqlServer(connectionString);
+            });
+        }
+
+        public static void ConfigureIdentity(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<DatabaseContext>()
+                .AddDefaultTokenProviders();
+        }
+
+        public static void ConfigureAuthentication(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            });
+        }
+
+        public static void ConfigureJwtBearer(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.SaveToken = true;
+                options.RequireHttpsMetadata = false;
+                options.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidAudience = builder.Configuration["JWT:ValidAudience"],
+                    ValidIssuer = builder.Configuration["JWT:ValidIssuer"],
+                    ClockSkew = TimeSpan.Zero,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Secret"]))
+                };
+            });
+        }
+
+        public static void ConfigureServices(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddTransient<ITokenService, TokenService>();
+        }
+    }
+}
